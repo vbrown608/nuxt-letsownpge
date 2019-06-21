296cd0b98a45db4e8934705bad4b3b51b96fdd31
Neil Ruud <neil@ruudmedia.com>
2019-06-20 20:51:07 -0700

    more hero fixes

--- components/sections/hero.vue
+++ components/sections/hero.vue
@@ -1,10 +1,13 @@
 <template>
-  <header class="min-h-1/2-screen flex flex-col bg-blue relative">
+  <header
+    v-if="content[0].hero_heading[0] != null"
+    class="min-h-1/2-screen flex flex-col bg-blue relative"
+  >
     <div class="w-full h-full absolute pin z-0">
       <imgix
         v-if="content[0].hero_image != null"
         objectfit="cover"
-        :originalurl="backgroundimage || content[0].hero_image.url"
+        :originalurl="content[0].hero_image.url"
         :params="imageParams"
         :aspectratios="['1:1', '16:9']"
       />
@@ -13,15 +16,15 @@
       <h1
         class="text-4xl md:text-5xl lg:text-6xl font-bold uppercase tracking-widest text-white text-center mb-10 leading-none"
       >
-        {{ content[0].hero_heading[0].text || heading }}
+        {{ content[0].hero_heading[0].text }}
       </h1>
       <h2
-        v-if="content[0].hero_subheading != null"
+        v-if="content[0].hero_subheading[0] != null"
         class="text-2xl text-white text-center mb-12 leading-none"
       >
-        {{ content[0].hero_subheading[0].text || subheading }}
+        {{ content[0].hero_subheading[0].text }}
       </h2>
-      <div v-if="content[0].hero_cta_link" class="text-center">
+      <div v-if="content[0].hero_cta_link[0] != null" class="text-center">
         <button-cta
           :cta="content[0].hero_cta_text"
           :link="content[0].hero_cta_link"
@@ -35,7 +38,7 @@
 export default {
   props: {
     content: {
-      type: Object,
+      type: Array,
       default: null
     }
   },
