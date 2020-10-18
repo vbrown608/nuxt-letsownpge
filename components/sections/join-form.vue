be28dc31a7f7aafa7917e5b4376af23128e97ecc
Vivian Brown <vivian@netlify.com>
2020-10-18 10:42:57 -0700

    Fix name

--- components/sections/join-form.vue
+++ components/sections/join-form.vue
@@ -45,7 +45,7 @@
     <div class="flex flex-wrap -mx-3 mb-10">
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="label" for="city"> City </label>
-        <input id="city" name="phone" class="input" type="text" />
+        <input id="city" name="city" class="input" type="text" />
       </div>
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="label" for="state"> State </label>
