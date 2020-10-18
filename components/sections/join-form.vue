9610287ae07f821081dcd8e04239ec550f88d8f5
Vivian Brown <vivian@netlify.com>
2020-10-18 00:36:36 -0700

    Add form name as hidden field

--- components/sections/join-form.vue
+++ components/sections/join-form.vue
@@ -7,6 +7,7 @@
     netlify
     netlify-honeypot="bot-field"
   >
+    <input type="hidden" name="form-name" value="join" />
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="label required" for="first-name"> First Name </label>
@@ -44,7 +45,7 @@
     <div class="flex flex-wrap -mx-3 mb-10">
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="label" for="city"> City </label>
-        <input id="city" name="phone" class="input" type="text" />
+        <input id="city" name="city" class="input" type="text" />
       </div>
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="label" for="state"> State </label>
