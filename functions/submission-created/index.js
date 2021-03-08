5be337e33d33d3f7d7e75cd4f0f5b6904ffa7c47
Vivian Brown <vivian@netlify.com>
2021-03-07 21:43:05 -0800

    Send confirmation email on submit

--- functions/submission-created/index.js
+++ functions/submission-created/index.js
@@ -1,19 +1,13 @@
+const fs = require('fs');
+
 const { GoogleSpreadsheet } = require('google-spreadsheet');
+const sgMail = require('@sendgrid/mail');
 
 exports.handler = async (event, context, callback) => {
   try {
-    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
-    await doc.useServiceAccountAuth({
-      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
-    });
-    await doc.loadInfo();
-    const sheet = doc.sheetsByIndex[0];
-
-    const req = JSON.parse(event.body);
-    const fields = req.payload.ordered_human_fields.map(o => o.value);
-    await sheet.addRow(fields);
-
+    const payload = JSON.parse(event.body).payload;
+    await addSubmissionToGoogleSheet(payload);
+    await sendConfirmationEmail(payload);
     return {
       statusCode: 200,
       body: JSON.stringify({
@@ -27,4 +21,39 @@ exports.handler = async (event, context, callback) => {
       body: e.toString(),
     };
   }
+};
+
+const addSubmissionToGoogleSheet = async function (payload) {
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
+  });
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[0];
+
+  const fields = payload.ordered_human_fields.map(o => o.value);
+  await sheet.addRow(fields);
+  console.log(`Added row to Google Sheet`);
+};
+
+const sendConfirmationEmail = function (payload) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const html = thanksHTML(payload.data.name);
+  const text = html.replace(/<[^>]*>/g, '');
+  const msg = {
+    to: payload.data.email,
+    from: 'vivian.brown@gmail.com',
+    subject: 'Thank you for joining us to own PG&E',
+    html,
+    text,
+  };
+  sgMail.send(msg).then(() => {
+    console.log(`Sent confirmation email`);
+  });
+};
+
+const thanksHTML = function (name) {
+  const tpl = fs.readFileSync('thanks.html');
+  return `<p>${name}</p>` + tpl;
 };
